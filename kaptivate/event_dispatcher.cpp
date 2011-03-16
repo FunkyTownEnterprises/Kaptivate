3ddd8563940dcc9a1fc179b2124f59ea9646293b
Chris Eberle <eberle1080@gmail.com>
2011-03-16 17:41:24 -0600

    Filled out the enumerate methods

--- kaptivate/event_dispatcher.cpp
+++ kaptivate/event_dispatcher.cpp
@@ -39,73 +39,118 @@
 using namespace std;
 using namespace Kaptivate;
 
+// Constructor
 EventDispatcher::EventDispatcher()
 {
     kdLock = CreateMutex(NULL, FALSE, NULL);
     mdLock = CreateMutex(NULL, FALSE, NULL);
 }
 
+// Destructor
 EventDispatcher::~EventDispatcher()
 {
     CloseHandle(kdLock);
     CloseHandle(mdLock);
 }
 
+// Dispatch a keyboard event to any registered handlers
 void EventDispatcher::handleKeyboard(KeyboardEvent& evt)
 {
 
 }
 
+// Dispatch a mouse button event to any registered handlers
 void EventDispatcher::handleMouseButton(MouseButtonEvent& evt)
 {
 }
 
+// Dispatch a mouse wheel event to any registered handlers
 void EventDispatcher::handleMouseWheel(MouseWheelEvent& evt)
 {
 }
 
+// Dispatch a mouse move event to any registered handlers
 void EventDispatcher::handleMouseMove(MouseMoveEvent& evt)
 {
 }
 
+// Get a list of attached keyboards
 vector<KeyboardInfo> EventDispatcher::enumerateKeyboards()
 {
-    vector<KeyboardInfo> dummy;
-    return dummy;
+    vector<KeyboardInfo> ret;
+    scanDevices();
+
+    {
+        ScopedMutex kMutex(kdLock);
+
+        map<HANDLE, KeyboardInfo*>::iterator it;
+        for(it = keyboardDevices.begin() ; it != keyboardDevices.end(); it++)
+        {
+            KeyboardInfo inf;
+            inf.device = (*it).second->device;
+            inf.name = (*it).second->name;
+            ret.push_back(inf);
+        }
+    }
+
+    return ret;
 }
 
+// Get a list of attached mice
 vector<MouseInfo> EventDispatcher::enumerateMice()
 {
-    vector<MouseInfo> dummy;
-    return dummy;
+    vector<MouseInfo> ret;
+    scanDevices();
+
+    {
+        ScopedMutex mMutex(mdLock);
+
+        map<HANDLE, MouseInfo*>::iterator it;
+        for(it = mouseDevices.begin() ; it != mouseDevices.end(); it++)
+        {
+            MouseInfo inf;
+            inf.device = (*it).second->device;
+            inf.name = (*it).second->name;
+            ret.push_back(inf);
+        }
+    }
+
+    return ret;
 }
 
+// Register a handler for keyboard events
 void EventDispatcher::registerKeyboardHandler(string idRegex, KeyboardHandler* handler)
 {
 }
 
+// Register a handler for mouse events
 void EventDispatcher::resgisterMouseHandler(string idRegex, MouseHandler* handler)
 {
 }
 
+// Unregister a handler for keyboard events
 void EventDispatcher::unregisterKeyboardHandler(KeyboardHandler* handler)
 {
 }
 
+// Unregister a handler for mouse events
 void EventDispatcher::unregisterMouseHandler(MouseHandler* handler)
 {
 }
 
+// Scan the raw devices and fill in the device info structures
 void EventDispatcher::scanDevices()
 {
     UINT nDevices = 0;
     PRAWINPUTDEVICELIST pRawInputDeviceList = NULL;
 
+    // Get the raw device count
     if(GetRawInputDeviceList(NULL, &nDevices, sizeof(RAWINPUTDEVICELIST)) != 0)
         throw KaptivateException("Call to GetRawInputDeviceList(1) failed");
 
     if(nDevices > 0)
     {
+        // Get the actual list of raw devices
         if((pRawInputDeviceList = (PRAWINPUTDEVICELIST)malloc(sizeof(RAWINPUTDEVICELIST) * nDevices)) == NULL)
             throw KaptivateException("Failed to allocate memory for the raw device list");
         if(GetRawInputDeviceList(pRawInputDeviceList, &nDevices, sizeof(RAWINPUTDEVICELIST)) == (UINT)-1)
@@ -122,6 +167,7 @@ void EventDispatcher::scanDevices()
         ScopedMutex mMutex(mdLock);
 
         {
+            // Clear out the keyboardDevices map
             map<HANDLE, KeyboardInfo*>::iterator it;
             for(it = keyboardDevices.begin() ; it != keyboardDevices.end(); it++)
                 delete (*it).second;
@@ -129,22 +175,23 @@ void EventDispatcher::scanDevices()
         }
 
         {
+            // Clear out the mouseDevices map
             map<HANDLE, MouseInfo*>::iterator it;
             for(it = mouseDevices.begin() ; it != mouseDevices.end(); it++)
                 delete (*it).second;
             mouseDevices.clear();
         }
 
+        // Iterate over the raw devices
         for(UINT i = 0; i < nDevices; i++)
         {
             RAWINPUTDEVICELIST& rid = pRawInputDeviceList[i];
             if(rid.dwType != RIM_TYPEKEYBOARD && rid.dwType != RIM_TYPEMOUSE)
                 continue;
-            if(keyboardDevices.count(rid.hDevice) > 0)
-                continue;
-            if(mouseDevices.count(rid.hDevice) > 0)
+            if(keyboardDevices.count(rid.hDevice) > 0 || mouseDevices.count(rid.hDevice) > 0)
                 continue;
 
+            // Get the length of the device name
             UINT pcbSize = 0;
             if(0 != GetRawInputDeviceInfo(rid.hDevice, RIDI_DEVICENAME, NULL, &pcbSize))
                 continue;
@@ -154,6 +201,7 @@ void EventDispatcher::scanDevices()
                 char* cDevName = (char*)malloc(sizeof(TCHAR) * pcbSize);
                 if(cDevName != NULL)
                 {
+                    // Get the device name
                     if(GetRawInputDeviceInfo(rid.hDevice, RIDI_DEVICENAME, (LPVOID)cDevName, &pcbSize) > 0)
                     {
                         string devName(cDevName); 
@@ -161,6 +209,7 @@ void EventDispatcher::scanDevices()
                         {
                             if(keyboardDevices.count(rid.hDevice) == 0)
                             {
+                                // Process the new keyboard device
                                 KeyboardInfo* kbi = new KeyboardInfo();
                                 kbi->device = rid.hDevice;
                                 kbi->name = devName;
@@ -171,6 +220,7 @@ void EventDispatcher::scanDevices()
                         {
                             if(mouseDevices.count(rid.hDevice))
                             {
+                                // Process the new mouse device
                                 MouseInfo* mi = new MouseInfo();
                                 mi->device = rid.hDevice;
                                 mi->name = devName;
